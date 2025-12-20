c79bcb76b304d6cd68c7e57a2af48f319e94de73
zirconium <zirconiumlee@gmail.com>
2025-12-20 12:16:15 +0800

    fix(database): open foreign key constraint

--- database/database.py
+++ database/database.py
@@ -6,8 +6,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DB_PATH = os.path.join(BASE_DIR, "data", "studyroom.db")
 
 def get_connection():
-    """获取数据库连接"""
     conn = sqlite3.connect(DB_PATH)
+    conn.execute("PRAGMA foreign_keys = ON;")
     return conn
 
 
